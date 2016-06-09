27f9b2345613c9f74540d9e476da4c3eccee4e1d
Minfeng Wen <minfengwen@hotmail.com>
2016-06-09 05:29:15 -0700

    change formats, add examples

--- RobotSimulation.hs
+++ RobotSimulation.hs
@@ -7,30 +7,32 @@ import Control.Monad.Trans.Maybe
 import Prelude hiding (drop)
 import Password 
 
--- ToDo: Based on the combinator pattern in our lecture, this should be changed to functions that return "Robot m a" as results
+-- Solved ToDo: Based on the combinator pattern in our lecture, this should be changed to functions that return "Robot m a" as results
 -- Reason: Such implemetation lacks of extensibility
+--
 data Action = MoveBy Pos
-      | Pickup Object
+      | PickUp Object
       | Drop
-      | Getdata
+      | GetData
       | DoNothing
---      | If Object Action 
+--    | If Object Action 
       deriving (Show,Eq)
 data Object = Sand | Stone | Empty deriving (Show,Eq)
 
 ifStone Sand = False
 
 
--- ToDo :Since Robot needs both State and IO effects, we can modularize this as a type class that should have 3 methods: 
+-- Solved ToDo :Since Robot needs both State and IO effects, we can modularize this as a type class that should have 3 methods: 
 -- 1. getState
 -- 2. putState
 -- 3. IO interaction
---class (MonadState s m, MonadIO m) => MonadStateIO s m where
+-- class (MonadState s m, MonadIO m) => MonadStateIO s m where
 
--- ToDo: How to add a type constraint like "sensor a" in the data type definition for Robot? 
+-- Solved ToDo: How to add a type constraint like "sensor a" in the data type definition for Robot? 
 -- data (Sensor b) => RobotState b = Robot (Energy, Pos, Load, Schedule, b) 
+--
 type Robot m a = StateT RobotState m a
-type RobotE a = Robot (MaybeT IO) a
+type RobotE a  = Robot (MaybeT IO) a
 
 
 type Energy = Int
@@ -41,12 +43,11 @@ type Schedule = [Action]
 data RobotState = RobotState { energy :: Energy, pos :: Pos, load :: Load, schedule :: Schedule, world :: World }
 
 instance Show RobotState where
-  show (RobotState e p l s w) = "Current Energy is " ++ show e ++
+  show (RobotState e p l s w) = "\nCurrent Energy is " ++ show e ++
                                 ".\nCurrent Position is " ++ show p ++
                                 ".\nCurrent Load is " ++ show l ++
                                 ".\nCurrent Schedule is " ++ show s ++
-                                ".\n\n" ++ show w 
-
+                                ".\n" ++ show w 
 
 data World = World { temperature :: Int, pressure :: Float, humidity :: Float, picture :: String }
 
@@ -54,9 +55,7 @@ instance Show World where
   show (World t p h ph) = "Temperature is " ++ show t ++
                          ".\nPressure is " ++ show p ++
                          ".\nHumidity is " ++ show h ++
-                         ".\nPrinting a picture : \n " ++ show ph 
-
-
+                         ".\nPrinting a picture: " ++ show ph 
 
 type Sensor a = World -> a
 
@@ -72,49 +71,45 @@ hygrometer w = humidity w
 camera :: Sensor String
 camera w = picture w
 
---moveTo :: Monad m => Pos -> Robot m ()
---moveTo i = do
---  (e,p,l,s)
-
 -- Since all the Sensors should have the same behavior: get data from the real world, it should be like IO Monad. 
 -- we want to use IO Monad to simulate it.
--- ToDo: 1. a is never used but helps to pass the typechecker
---       2. Whether there is a nice way?
---class Sensor a where
---  getInfo :: a -> IO String 
+-- Solved ToDo: 1. a is never used but helps to pass the typechecker
+--              2. Whether there is a nice way?
+-- class Sensor a where
+-- getInfo :: a -> IO String 
 
---instance Sensor Thermometer where
---  getInfo a = getLine
+-- instance Sensor Thermometer where
+-- getInfo a = getLine
 
---data Thermometer = Thermometer
+-- data Thermometer = Thermometer
 
---instance Sensor Hygrometer where
---  getInfo a = getLine
+-- instance Sensor Hygrometer where
+-- getInfo a = getLine
 
---data Hygrometer = Hygrometer
+-- data Hygrometer = Hygrometer
 
---instance Sensor Barometer where
---  getInfo a = getLine
+-- instance Sensor Barometer where
+-- getInfo a = getLine
 
---data Barometer = Barometer
+-- data Barometer = Barometer
 
---instance Sensor Camera where
---  getInfo a = getLine
+-- instance Sensor Camera where
+-- getInfo a = getLine
 
---data Camera = Camera
+-- data Camera = Camera
 
 run :: Show a => Energy -> Pos -> RobotE a -> IO ()
 run e p m = do 
   a <- runMaybeT (runStateT m (RobotState e p Empty [] (World 0 0.0 0.0 "")))
   case a of
-    Just (a,s) -> putStrLn ("Final state is " ++ show s ++ ".")
+    Just (a,s) -> putStrLn ("\nFinal state: " ++ show s ++ ".")
     Nothing -> putStrLn ""
 
 runAction :: Action -> RobotE ()
 runAction (MoveBy p) = moveBy p
-runAction (Pickup o) = pickUp o
+runAction (PickUp o) = pickUp o
 runAction Drop = drop
-runAction Getdata = getWorld >> return ()
+runAction GetData = getWorld >> return ()
 runAction DoNothing = return () 
 
 runSchedule :: Schedule -> RobotE ()
@@ -125,7 +120,7 @@ run' :: Energy -> Pos -> Schedule -> IO ()
 run' e p sc = do 
   a <- runMaybeT (runStateT (runSchedule sc) (RobotState e p Empty [] (World 0 0.0 0.0 "")))
   case a of
-    Just (a,s) -> putStrLn ("Final state is " ++ show s ++ ".")
+    Just (a,s) -> putStrLn ("\nFinal State: " ++ show s ++ ".")
     Nothing -> putStrLn ""
 
 moveBy :: (Int, Int) -> RobotE ()
@@ -137,36 +132,39 @@ moveBy (i,j) = do
         setEnergy (e-i-j)
         setPos (px+i, py+j)
       else 
-        ((lift.lift.putStrLn) ("Error: Not Enough Energy!" ++ "Current Energy is " ++ show e ++ ", need reCharge 10 ? ") >> 
+        ((lift.lift.putStrLn) ("Error: Not Enough Energy!" ++ " Current Energy is " ++ show e ++ ", need reCharge 10 ? ") >> 
         lift newGoodPass >>= 
                           (\a -> if a == 'Y' 
                                  then setEnergy (e+10) >> moveBy (i,j)
                                  else ( get >>= (\currentState -> (lift.lift.putStrLn.show) currentState) >> lift mzero)))
---  (e,(px,py),l,s) <- get
---if i+j <e then put (e-i-j,(px+i,py+j),l,s)
-  --RobotState e (px,py) l s w <- get
-  --if i+j <e 
-  --then put (RobotState (e-i-j) (px+i,py+j) l s w)
-  --else  
-  --    ((lift.lift.putStrLn) ("Error: Not Enough Energy!" ++ "Current Energy is " ++ show e ++ ", need reCharge 10 ? ") >> 
-  --     lift newGoodPass >>= 
-  --                        (\a -> if a == 'Y' 
-  --                               then put (RobotState (e+10) (px,py) l s w) >> moveBy (i,j)
-  --                               else ( get >>= (\currentState -> (lift.lift.putStrLn.show) currentState) >> lift mzero)))
+
+-- (e,(px,py),l,s) <- get
+-- if i+j <e then put (e-i-j,(px+i,py+j),l,s)
+-- RobotState e (px,py) l s w <- get
+-- if i+j <e 
+-- then put (RobotState (e-i-j) (px+i,py+j) l s w)
+-- else  
+--     ((lift.lift.putStrLn) ("Error: Not Enough Energy!" ++ "Current Energy is " ++ show e ++ ", need reCharge 10 ? ") >> 
+--      lift newGoodPass >>= 
+--                         (\a -> if a == 'Y' 
+--                                then put (RobotState (e+10) (px,py) l s w) >> moveBy (i,j)
+--                                else ( get >>= (\currentState -> (lift.lift.putStrLn.show) currentState) >> lift mzero)))
+
+-- moveTo :: Monad m => Pos -> Robot m ()
+-- moveTo i = do
+--   (e,p,l,s)
 
 pickUp :: Object -> RobotE ()
 pickUp o = do
   l <- getLoad
   if l == Empty then setLoad o
-                else lift . lift . putStrLn $ "Error: Already have an object!"
+                else lift . lift . putStrLn $ "Error: Already has an object!"
 
---  (e,p,l,s) <- get
---if l == Empty then put (e,p,o,s)
-  --RobotState e p l s w <- get
---  if l == Empty then put (RobotState e p o s w)
---                else lift.lift.putStrLn $ "Error: Already have an object!"
-
---stateT maybeT IO
+-- (e,p,l,s) <- get
+-- if l == Empty then put (e,p,o,s)
+-- RobotState e p l s w <- get
+-- if l == Empty then put (RobotState e p o s w)
+--               else lift.lift.putStrLn $ "Error: Already have an object!"
 
 drop :: RobotE ()
 drop = do
@@ -178,44 +176,44 @@ setEnergy e = do
   put (RobotState e p l s w)
 
 getEnergy :: RobotE Energy
---getEnergy = do
---  RobotState e _ _ _ _ <- get
---  return e
+-- getEnergy = do
+-- RobotState e _ _ _ _ <- get
+-- return e
 getEnergy = do
   e <- liftM energy get
   lift.lift.putStrLn.show $ e
   return e
 
---printEnergy :: Robot IO ()
-----printEnergy = do
-----RobotState e _ _ _ _ <- get
-----lift $ putStrLn (show e)
---printEnergy = get >>= (lift.putStrLn.show.energy)
+-- printEnergy :: Robot IO ()
+-- --printEnergy = do
+-- --RobotState e _ _ _ _ <- get
+-- --lift $ putStrLn (show e)
+-- printEnergy = get >>= (lift.putStrLn.show.energy)
 
 setPos :: Pos -> RobotE ()
 setPos p = do
   RobotState e _ l s w <- get
   put (RobotState e p l s w)
 
-setLoad :: Load -> RobotE ()
-setLoad l = do
-  RobotState e p _ s w <- get
-  put (RobotState e p l s w)
-
 getPos :: RobotE Pos
---getPos = do
---  RobotState _ p,_,_) <- get
---  return p
+-- getPos = do
+--   RobotState _ p,_,_) <- get
+--   return p
 getPos = do
   p <- liftM pos get
   lift.lift.putStrLn.show $ p
   return p
 
---printPos :: Robot IO ()
-----printPos = do
-----  (_,p,_,_) <- get
-----  lift $ putStrLn (show p)
---printPos = get >>= (lift.putStrLn.show.pos)
+-- printPos :: Robot IO ()
+-- --printPos = do
+-- --  (_,p,_,_) <- get
+-- --  lift $ putStrLn (show p)
+-- printPos = get >>= (lift.putStrLn.show.pos)
+
+setLoad :: Load -> RobotE ()
+setLoad l = do
+  RobotState e p _ s w <- get
+  put (RobotState e p l s w)
 
 getLoad :: RobotE Load
 --getLoad = do
@@ -226,11 +224,11 @@ getLoad = do
   lift.lift.putStrLn.show $ l
   return l
 
---printLoad :: Robot IO ()
-----printLoad = do
-----  (_,_,l,_) <- get
-----  lift $ putStrLn (show l)
---printLoad = get >>= (lift.putStrLn.show.load)
+-- printLoad :: Robot IO ()
+-- --printLoad = do
+-- --  (_,_,l,_) <- get
+-- --  lift $ putStrLn (show l)
+-- printLoad = get >>= (lift.putStrLn.show.load)
 
 setSchedule :: Schedule -> RobotE ()
 setSchedule s = do
@@ -238,21 +236,19 @@ setSchedule s = do
   put (RobotState e p l s w)
 
 getSchedule :: RobotE Schedule
---getSchedule = do
---  (_,_,_,s) <- get
---  return s
+-- getSchedule = do
+--   (_,_,_,s) <- get
+--   return s
 getSchedule = do
   s <- liftM schedule get
   lift . lift . putStrLn . show $ s
   return s
 
-
---TODO fix this
---printSchedule :: Robot IO ()
-----printSchedule = do
-----  (_,_,_,s) <- get
-----  lift $ putStrLn (show s)
---printSchedule = get >>= (lift.putStrLn.show.schedule)
+-- printSchedule :: Robot IO ()
+-- --printSchedule = do
+-- --  (_,_,_,s) <- get
+-- --  lift $ putStrLn (show s)
+-- printSchedule = get >>= (lift.putStrLn.show.schedule)
 
 setWorld :: World -> RobotE ()
 setWorld w = do
@@ -265,9 +261,8 @@ getWorld = do
   lift . lift . putStrLn . show $ w
   return w
 
-
---printWorld :: Robot IO ()
---printWorld = get >>= (lift.putStrLn.show.world)
+-- printWorld :: Robot IO ()
+-- printWorld = get >>= (lift.putStrLn.show.world)
 
 addAction :: Action -> RobotE ()
 addAction a = do
@@ -338,3 +333,70 @@ getPicture = do
   lift . lift . putStrLn . show $ p
   return p
 
+-- Example 1:
+-- >>> run 20 (1,1) $ moveBy (10,10) >> pickUp Sand >> getWorld
+-- 20
+-- (1,1) 
+-- Error: Not Enough Energy! Current Energy is 20, need reCharge 10 ?
+-- Input Y/N
+-- >>> Y
+-- 30
+-- (1,1)
+-- Empty
+-- Temperature is 0.
+-- pressure is 0.0.
+-- Humidity is 0.0.
+-- Printing a picture: ""
+--
+-- Final State:
+-- Current Energy is 10.
+-- Current Position is (11,11).
+-- Current Load is Sand.
+-- Current Schedule is [].
+-- Temperature is 0.
+-- Pressure is 0.0.
+-- Humidity is 0.0.
+-- Printing a picture: "".
+
+-- Example 2:
+-- >>> run' 20 (1,1) [MoveBy (9,9), PickUp Sand, PickUp Stone, GetData]
+-- 20
+-- (1,1)
+-- Empty
+-- Sand
+-- Error: Already has an object!
+-- Temperature is 0.
+-- pressure is 0.0.
+-- Humidity is 0.0.
+-- Printing a picture: ""
+--
+-- Final State:
+-- Current Energy is 2.
+-- Current Position is (10,10).
+-- Current Load is Sand.
+-- Current Schedule is [].
+-- Temperature is 0.
+-- Pressure is 0.0.
+-- Humidity is 0.0.
+-- Printing a picture: "".
+
+-- Example 3:
+-- >>> run 20 (1,1) $ moveBy (2,2) >> moveBy (5,5) >> moveBy (7,7) >> pickUp Stone
+-- 20
+-- (1,1)
+-- 16
+-- (3,3)
+-- 6
+-- (8,8)
+-- Error: Not Enough Energy! Current Energy is 20, need reCharge 10 ?
+-- Input Y/N
+-- >>> N
+-- 
+-- Current Energy is 6.
+-- Current Position is (8,8).
+-- Current Load is Empty.
+-- Current Schedule is [].
+-- Temperature is 0.
+-- Pressure is 0.0.
+-- Humidity is 0.0.
+-- Printing a picture: ""
